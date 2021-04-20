76ba898f4217fbc44c241ef990f6940bd42c8dc6
jjuanramos <jjuanramos91@gmail.com>
2021-04-20 19:42:40 +0200

    Initial commit

--- kiva_pipeline/app.py
+++ kiva_pipeline/app.py
@@ -3,10 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return 'OK'
+    return "OK"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
--- tests/test_kiva_pipeline.py
+++ tests/test_kiva_pipeline.py
@@ -1,16 +1,16 @@
-from app import app
 import pytest
+from app import app
 
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
 
     with app.test_client() as client:
         yield client
 
 
 def test_home(client):
-    response = client.get('/')
+    response = client.get("/")
 
     assert response.status_code == 200
