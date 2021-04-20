3f57ee68a11b3509f80817bd4644baaa9d7c7ac5
jjuanramos <jjuanramos91@gmail.com>
2021-04-20 19:39:44 +0200

    first commit

---
+++ tests/test_kiva_pipeline.py
@@ -0,0 +1,16 @@
+from app import app
+import pytest
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+
+    with app.test_client() as client:
+        yield client
+
+
+def test_home(client):
+    response = client.get('/')
+
+    assert response.status_code == 200
