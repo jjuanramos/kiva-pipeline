50a4e038f3f1b81414aea4feac4bd8242cb3c71b
jjuanramos <jjuanramos91@gmail.com>
2021-04-20 20:57:44 +0200

    added base graphql model

--- kiva_pipeline/app.py
+++ kiva_pipeline/app.py
@@ -1,11 +1,47 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
+
+BASE_URL = "https://api.kivaws.org/graphql?query="
 
 
 @app.route("/")
 def home() -> str:
-    return "OK"
+    return "Maria BITCH"
+
+
+@app.route("/kiva")
+def kiva_get() -> dict:
+    graphql_query = """
+        {
+            lend {
+                loans(sortBy: newest, limit: 5) {
+                    values {
+                        id
+                        loanAmount
+                        status
+                        researchScore
+                        repaymentInterval
+                        endorser {
+                            id
+                        }
+                        distributionModel
+                        terms {
+                            currencyFullName
+                            disbursalAmount
+                            disbursalDate
+                            flexibleFundraisingEnabled
+                        }
+                        loanAmount
+                        paidAmount
+                    }
+                }
+            }
+        }
+    """
+    r = requests.get(BASE_URL+graphql_query)
+    return r.json()
 
 
 if __name__ == "__main__":
