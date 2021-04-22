ca7e9555c4f01bf2e7309b9b391a2020cf3d5b6a
jjuanramos <jjuanramos91@gmail.com>
2021-04-22 20:36:16 +0200

    added tests, route to get item

--- kiva_pipeline/app.py
+++ kiva_pipeline/app.py
@@ -7,13 +7,20 @@ BASE_URL = "https://api.kivaws.org/graphql?query="
 
 
 @app.get("/")
-async def home() -> str:
-    return "Hello there!"
+def home() -> tuple:
+    return "Hello there!", 200
 
 
-@app.get("/get_n_kiva_loans")
-def kiva_get(limit_number: int = 5) -> dict:
-    query = """
+@app.get("/get_kiva_batch")
+def reroute_batch() -> dict:
+    return {
+        "message": "You need to provide a number such as /get_kiva_batch/2"
+    }
+
+
+@app.get("/get_kiva_batch/{limit_number}")
+def get_kiva_batch(limit_number: int = 5) -> dict:
+    batch_query = """
         query ($limit_number: Int)
         {
             lend {
@@ -49,6 +56,63 @@ def kiva_get(limit_number: int = 5) -> dict:
         "limit_number": limit_number
     }
 
-    r = requests.post(BASE_URL, json={'query': query, 'variables': variables})
+    r = requests.post(
+        BASE_URL,
+        json={
+            "query": batch_query,
+            "variables": variables
+        }
+    )
+
+    return r.json()
+
+
+@app.get("/get_kiva_item")
+def reroute_item() -> dict:
+    return {
+        "message": "You need to provide an id such as /get_kiva_item/50000"
+    }
+
+
+@app.get("/get_kiva_item/{id}")
+def get_kiva_item(id: int) -> dict:
+    item_query = """
+        query($id: Int!)
+        {
+            lend {
+                loan(id: $id) {
+                id
+                loanAmount
+                status
+                researchScore
+                repaymentInterval
+                endorser {
+                    id
+                }
+                distributionModel
+                terms {
+                    currencyFullName
+                    disbursalAmount
+                    disbursalDate
+                    flexibleFundraisingEnabled
+                }
+                loanAmount
+                paidAmount
+                }
+            }
+        }
+    """
+
+    variables = {
+        "id": id
+    }
+
+    r = requests.post(
+        BASE_URL,
+        json={
+            "query": item_query,
+            "variables": variables
+        }
+    )
 
     return r.json()
--- tests/test_kiva_pipeline.py
+++ tests/test_kiva_pipeline.py
@@ -1,16 +1,77 @@
-import pytest
-from kiva_pipeline import app
+import requests
 
 
-@pytest.fixture
-def client():
-    app.config["TESTING"] = True
-
-    with app.test_client() as client:
-        yield client
-
-
-def test_home(client):
-    response = client.get("/")
+def test_basic_response():
+    response = requests.get("http://127.0.0.1:8000/")
 
     assert response.status_code == 200
+
+
+def test_get_kiva_item_empty():
+    response = requests.get("http://127.0.0.1:8000/get_kiva_item/")
+
+    assert type(response.json()) == dict
+    assert "message" in response.json()
+
+
+def test_get_kiva_item_response_given_str():
+    response = requests.get("http://127.0.0.1:8000/get_kiva_item/'s'")
+
+    assert type(response.json()) == dict
+    assert "detail" in response.json()
+    assert response.json()\
+        .get('detail')[0].get("msg") == "value is not a valid integer"
+
+
+def test_get_kiva_item_response_given_int():
+    response = requests.get("http://127.0.0.1:8000/get_kiva_item/50000")
+
+    assert type(response.json()) == dict
+    assert "detail" not in response.json()
+    assert "data" in response.json()
+
+
+def test_get_kiva_batch_response_given_str():
+    response = requests.get("http://127.0.0.1:8000/get_kiva_batch/'s'")
+
+    assert type(response.json()) == dict
+    assert "detail" in response.json()
+    assert response.json()\
+        .get('detail')[0].get("msg") == "value is not a valid integer"
+
+
+def test_get_kiva_batch_response_given_int():
+    response = requests.get("http://127.0.0.1:8000/get_kiva_batch/5")
+
+    assert type(response.json()) == dict
+    assert "detail" not in response.json()
+    assert "data" in response.json()
+    assert len(
+        response.json()
+        .get("data")
+        .get("lend")
+        .get("loans")
+        .get("values")
+    ) == 5
+
+
+def test_get_kiva_batch_response_given_empty():
+    response = requests.get("http://127.0.0.1:8000/get_kiva_batch")
+
+    assert type(response.json()) == dict
+    assert "message" in response.json()
+
+
+def test_get_kiva_batch_response_given_int_greater_than_twenty():
+    response = requests.get("http://127.0.0.1:8000/get_kiva_batch/30")
+
+    assert type(response.json()) == dict
+    assert "detail" not in response.json()
+    assert "data" in response.json()
+    assert len(
+        response.json()
+        .get("data")
+        .get("lend")
+        .get("loans")
+        .get("values")
+        ) == 20
